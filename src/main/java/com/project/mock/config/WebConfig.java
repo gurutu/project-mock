16e9ee46b4946783d34ca78ed1f93928056451b9
pranav2014 <pranavyadav2014@gmail.com>
2020-02-02 22:53:17 +0530

    Added CORS

--- src/main/java/com/project/mock/config/WebConfig.java
+++ src/main/java/com/project/mock/config/WebConfig.java
@@ -1,13 +1,15 @@
 package com.project.mock.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -21,6 +23,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                         "classpath:/static/img/",
                         "classpath:/static/css/",
                         "classpath:/static/js/");
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
