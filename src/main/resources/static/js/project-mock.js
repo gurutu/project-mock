c9bbe10922023da6414dbbefd887abcd60a6338b
pranav2014 <pranavyadav2014@gmail.com>
2020-02-07 19:26:42 +0530

    change CSS style

--- src/main/resources/static/js/project-mock.js
+++ src/main/resources/static/js/project-mock.js
@@ -57,6 +57,56 @@ app.controller('apidashboardCtrl', function($scope) {
 });
 
 
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('orange')
+    //.dark();
+    .backgroundPalette('blue-grey');
+//	$mdThemingProvider.theme('default')
+//    .dark();
+	// $mdThemingProvider.disableTheming();
+//	 $mdThemingProvider.theme('altTheme')
+//	    .primaryPalette('purple') 
+	//$mdThemingProvider.generateThemesOnDemand(true);
+
+	  //themes are still defined in config, but the css is not generated
+//	  $mdThemingProvider.theme('altTheme')
+//	    .primaryPalette('blue')
+//	    .accentPalette('green');
+	 // Enable browser color
+//    $mdThemingProvider.enableBrowserColor({
+//      theme: 'default', // Default is 'default'
+//      palette: 'accent', // Default is 'primary', any basic material palette and extended palettes are available
+//      hue: '200' // Default is '800'
+//    });
+//	  $mdThemingProvider.definePalette('amazingPaletteName', {
+//		    '50': 'ffebee',
+//		    '100': 'ffcdd2',
+//		    '200': 'ef9a9a',
+//		    '300': 'e57373',
+//		    '400': 'ef5350',
+//		    '500': 'f44336',
+//		    '600': 'e53935',
+//		    '700': 'd32f2f',
+//		    '800': 'c62828',
+//		    '900': 'b71c1c',
+//		    'A100': 'ff8a80',
+//		    'A200': 'ff5252',
+//		    'A400': 'ff1744',
+//		    'A700': 'd50000',
+//		    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+//		                                        // on this palette should be dark or light
+//
+//		    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+//		     '200', '300', '400', 'A100'],
+//		    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+//		  });
+//
+//		  $mdThemingProvider.theme('default')
+//		    .primaryPalette('amazingPaletteName')
+});
+
 app.directive("requestResponseTemplate", function() {
     return {
     	templateUrl: 'request-response-template.html'//'request-response-template.html'
